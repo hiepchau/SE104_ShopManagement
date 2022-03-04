75e1670bac019543712335df914894ad6992e5b9
unknown <nhtdforwork@gmail.com>
2022-03-04 21:01:29 +0700

    Bulding models entity

---
+++ SE104_project/Models/Entity/UserBase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SE104_OnlineShopManagement.Models.Entity
+{
+    public enum UserRole
+    {
+        Owner = 0,
+        Manager = 1,
+        staff = 2
+    }
+    public class UserInfo
+    {
+        public string Id { get; private set; }
+        public string Name { get; private set; }
+        public int Age { get; private set; }
+        public UserRole Role { get; private set; }
+        public string Address { get; private set; }
+    }
+}
--- SE104_project/Services/Services.cs
+++ SE104_project/Services/Services.cs
@@ -3,11 +3,20 @@ using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using SE104_OnlineShopManagement.Services.Common;
 
 namespace SE104_OnlineShopManagement.Services
 {
     public static class Services
     {
+        public static IHostBuilder AddStores(this IHostBuilder host)
+        {
+            host.ConfigureServices(services =>
+            {
+                services.AddSingleton<IViewState, ViewState>();
+            });
+            return host;
+        }
         public static IHostBuilder AddModels(this IHostBuilder host)
         {
             return null;
