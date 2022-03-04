9769d3313c61f71c407510a22d3c1883cd3ce686
unknown <nhtdforwork@gmail.com>
2022-03-04 20:30:55 +0700

    Add View state

---
+++ SE104_project/Commands/UpdateCurrentViewModelCommand.cs
@@ -0,0 +1,33 @@
+﻿using SE104_OnlineShopManagement.Services.Common;
+using SE104_OnlineShopManagement.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace SE104_OnlineShopManagement.Commands
+{
+    public class UpdateCurrentViewModelCommand<TViewModel> : ICommand where TViewModel : ViewModelBase
+    {
+        public event EventHandler CanExecuteChanged;
+
+        private readonly ViewState _viewState;
+        private readonly IViewModelFactory _viewModelFactory;
+
+        public UpdateCurrentViewModelCommand(ViewState viewState, IViewModelFactory viewModelFactory)
+        {
+            _viewState = viewState;
+            _viewModelFactory = viewModelFactory;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter = null)
+        {
+            _viewState.CurrentMainViewModel = _viewModelFactory.CreateViewModel<TViewModel>();
+        }
+    }
+}
