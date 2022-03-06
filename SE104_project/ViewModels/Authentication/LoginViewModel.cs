fcc5375c1539f8a2c3ba219b51d62afce9600ece
unknown <nhtdforwork@gmail.com>
2022-03-07 02:21:23 +0700

    Update interaction

--- SE104_project/ViewModels/Authentication/LoginViewModel.cs
+++ SE104_project/ViewModels/Authentication/LoginViewModel.cs
@@ -1,20 +1,34 @@
 ﻿using SE104_OnlineShopManagement.Commands;
+using SE104_OnlineShopManagement.Services.Common;
+using SE104_OnlineShopManagement.Views.Windows;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text;
+using System.Windows;
+using System.Windows.Input;
 
 namespace SE104_OnlineShopManagement.ViewModels.Authentication
 {
     public class LoginViewModel:ViewModelBase
     {
         private IViewModelFactory _viewModelFactory;
+        private Window _authenticationWindow;
+        public ICommand RegisterCommand1 { get;private set; }   
         public UpdateCurrentViewModelCommand<RegisterViewModel> RegisterCommand { get; set; }
-        public LoginViewModel(IViewModelFactory factory)
+        public LoginViewModel(IViewModelFactory factory, AuthenticationWindow authen)
         {
             _viewModelFactory = factory;
+            _authenticationWindow = authen;
             RegisterCommand = new UpdateCurrentViewModelCommand<RegisterViewModel>(factory.CreateViewModel<MainViewModel>(), factory);
+            RegisterCommand1 = new RelayCommand<object>(null, OpenRegister);
         }
 
-        
+        public void OpenRegister(object o=null)
+        {
+            Trace.WriteLine("Command executed");
+            _viewModelFactory.CreateViewModel<MainViewModel>().CurrentMainViewModel = _viewModelFactory.CreateViewModel<RegisterViewModel>();
+            _authenticationWindow.DataContext = _viewModelFactory.CreateViewModel<MainViewModel>();
+        }
     }
 }
