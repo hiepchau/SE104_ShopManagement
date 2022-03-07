9c1408af5ddfdb93762039a2e3ee5fb5d475430c
hiepc <ch.dhiep@gmail.com>
2022-03-07 10:25:39 +0700

    Update UI

---
+++ SE104_project/ViewModels/NavigationMenuViewModel/ManagementViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SE104_OnlineShopManagement.ViewModels.NavigationMenuViewModel
+{
+    public class ManagementViewModel
+    {
+        public string Title { get; } = "Management";
+    }
+}
---
+++ SE104_project/ViewModels/NavigationMenuViewModel/ReportViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SE104_OnlineShopManagement.ViewModels.NavigationMenuViewModel
+{
+    public class ReportViewModel
+    {
+        public string Title { get; } = "Report";
+
+    }
+}
