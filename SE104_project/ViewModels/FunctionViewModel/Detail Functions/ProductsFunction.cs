13736bdc6f887259e95b8c5e4af1c2603c32ab73
namkhanh685 <trannamkhanh685@gmail.com>
2022-04-21 16:19:17 +0700

    Update SaveCommand

--- SE104_project/ViewModels/FunctionViewModel/Detail Functions/ProductsFunction.cs
+++ SE104_project/ViewModels/FunctionViewModel/Detail Functions/ProductsFunction.cs
@@ -73,7 +73,7 @@ namespace SE104_OnlineShopManagement.ViewModels.FunctionViewModel.Detail_Functio
             long cost =long.Parse(values[3].ToString());
             long price = long.Parse(values[4].ToString());
             ProductsInformation info = new ProductsInformation("", values[0].ToString(), 1, price, cost,
-                "", "","");
+                values[1].ToString(), "",values[2].ToString());
             RegisterProducts regist = new RegisterProducts(info, _connection.client, _session);
             string s = regist.register();
             Console.WriteLine(s);
--- SE104_project/ViewModels/FunctionViewModel/Detail Functions/ProductsTypeFunction.cs
+++ SE104_project/ViewModels/FunctionViewModel/Detail Functions/ProductsTypeFunction.cs
@@ -1,18 +1,37 @@
 ﻿using SE104_OnlineShopManagement.Models.ModelEntity;
 using SE104_OnlineShopManagement.Network;
+using SE104_OnlineShopManagement.Commands;
+using SE104_OnlineShopManagement.Network.Insert_database;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
 
 namespace SE104_OnlineShopManagement.ViewModels.FunctionViewModel.Detail_Functions
 {
     class ProductsTypeFunction:BaseFunction
     {
+        private MongoConnect _connection;
+        private AppSession _session;
+        #region
+        public ICommand SaveCommand { get; set; }
+        #endregion
         public List<ProductTypeInfomation> listItemsProductType { get; set; }
         public ProductsTypeFunction(AppSession session, MongoConnect connect) : base(session, connect)
         {
+            this._connection = connect;
+            this._session = session;
             listItemsProductType = new List<ProductTypeInfomation>();
             listItemsProductType.Add(new ProductTypeInfomation("1", "Nuoc giai khat"));
+            SaveCommand = new RelayCommand<Object>(null, SaveProductType);
+        }
+        public void SaveProductType(object parameter)
+        {
+            var values=(object[])parameter;
+            ProductTypeInfomation info = new ProductTypeInfomation("",values[0].ToString(),values[1].ToString());
+            RegisterProductType regist = new RegisterProductType(info, _connection.client, _session);
+            string s = regist.register();
+            Console.WriteLine(s);
         }
     }
 }
