46622466b30506fadc4dd4036bb3938df8368cb7
hiepc <ch.dhiep@gmail.com>
2022-05-02 11:57:54 +0700

    Fix POS ui

---
+++ SE104_project/Services/Validations/NotNullValidation.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SE104_OnlineShopManagement.Services.Validations
+{
+    class NotNullValidation
+    {
+
+    }
+}
