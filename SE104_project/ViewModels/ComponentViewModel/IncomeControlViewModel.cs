4c49c0f12a012b5a60e38ac341cfd867f14fc230
namkhanh685 <trannamkhanh685@gmail.com>
2022-05-17 21:41:22 +0700

    Add IncomeFunction

---
+++ SE104_project/ViewModels/ComponentViewModel/IncomeControlViewModel.cs
@@ -0,0 +1,52 @@
+﻿using MongoDB.Driver;
+using SE104_OnlineShopManagement.Models.ModelEntity;
+using SE104_OnlineShopManagement.Network.Get_database;
+using SE104_OnlineShopManagement.ViewModels.FunctionViewModel;
+using SE104_OnlineShopManagement.ViewModels.FunctionViewModel.Detail_Functions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SE104_OnlineShopManagement.ViewModels.ComponentViewModel
+{
+    public class IncomeControlViewModel: ViewModelBase
+    {
+        #region Properties
+        public BillInformation bill { get; set; }
+        public string ID { get; set; }
+        public DateTime saleDay { get; set; }
+        public string User { get; set; }
+        public long total { get; set; }
+        public string displayID { get; set; }
+        private IIncomeParent _parent;
+        #endregion
+        public IncomeControlViewModel(BillInformation billinfo, IIncomeParent parent)
+        {
+            this.bill = billinfo;
+            ID=billinfo.ID;
+            saleDay = billinfo.saleDay;
+            total = billinfo.total;
+            displayID = billinfo.displayID;
+            _parent = parent;
+            GetEmployeeName();
+        }
+        #region Function
+        public async void GetEmployeeName()
+        {
+            var filter = Builders<UserInfomation>.Filter.Eq(x => x.ID, bill.User);
+            GetUsers getter = new GetUsers((_parent as BaseFunction).Connect.client, (_parent as BaseFunction).Session, filter);
+            var ls = await getter.get();
+            if (ls != null && ls.Count > 0)
+            {
+                User = ls.First().LastName;
+                OnPropertyChanged(nameof(User));
+            }
+            else
+            {
+                return;
+            }
+        }
+        #endregion
+    }
+}
--- SE104_project/ViewModels/FunctionViewModel/Detail Functions/IncomeFunction.cs
+++ SE104_project/ViewModels/FunctionViewModel/Detail Functions/IncomeFunction.cs
@@ -1,16 +1,53 @@
-﻿using SE104_OnlineShopManagement.Models.ModelEntity;
+﻿using MongoDB.Driver;
+using SE104_OnlineShopManagement.Models.ModelEntity;
 using SE104_OnlineShopManagement.Network;
+using SE104_OnlineShopManagement.Network.Get_database;
+using SE104_OnlineShopManagement.ViewModels.ComponentViewModel;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace SE104_OnlineShopManagement.ViewModels.FunctionViewModel.Detail_Functions
 {
-    class IncomeFunction : BaseFunction
+    public interface IIncomeParent
     {
+
+    }
+    class IncomeFunction : BaseFunction, IIncomeParent
+    {
+        #region Properties
+        private MongoConnect _connection;
+        private AppSession _session;
+        public bool isLoaded { get; set; }
+        public ObservableCollection<IncomeControlViewModel> listIncome { get; set; }
+        #endregion
         public IncomeFunction(AppSession session, MongoConnect connect) : base(session, connect)
         {
-
+            this._session = session;
+            this._connection = connect;
+            isLoaded = true;
+            listIncome = new ObservableCollection<IncomeControlViewModel>();
+            GetData();
         }
+        #region DB
+        public async void GetData()
+        {
+            var filter = Builders<BillInformation>.Filter.Empty;
+            GetBills getter = new GetBills(_connection.client, _session, filter);
+            Task<List<BillInformation>> task = getter.Get();
+            var ls = await task;
+            Task.WaitAll(task);
+            foreach (BillInformation bill in ls)
+            {
+                listIncome.Add(new IncomeControlViewModel(bill, this));
+            }
+            isLoaded = false;
+            OnPropertyChanged(nameof(isLoaded));
+            Console.Write("Executed");
+            OnPropertyChanged(nameof(listIncome));
+        }
+        #endregion
     }
 }
