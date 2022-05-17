7df72b3d5094a207997bff117cfacab65ae91196
hiepc <ch.dhiep@gmail.com>
2022-05-17 17:20:06 +0700

    Binding OrdersPage

---
+++ SE104_project/ViewModels/ComponentViewModel/OrdersControlViewModel.cs
@@ -0,0 +1,39 @@
+﻿using SE104_OnlineShopManagement.Models.ModelEntity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace SE104_OnlineShopManagement.ViewModels.ComponentViewModel
+{
+    class OrdersControlViewModel : ViewModelBase
+    {
+        #region Properties
+        public BillInformation billInformation { get; set; }
+        public string ID { get; set; }
+        public DateTime saleDay { get; set; }
+        public string User { get; set; }
+        public string customer { get; set; }
+        public long total { get; set; }
+        public string displayID { get; set; }
+        #endregion
+
+        #region ICommand
+        public ICommand ViewOrdersCommand { get; set; }
+        #endregion
+
+        public OrdersControlViewModel(BillInformation bill)
+        {
+            this.billInformation = bill;
+            ID = bill.ID;
+            saleDay = bill.saleDay;
+            User = bill.User;
+            customer = bill.customer;
+            total = bill.total;
+            displayID = bill.displayID;
+        }
+
+        #region Function
+        #endregion
+    }
+}
--- SE104_project/ViewModels/FunctionViewModel/Detail Functions/OrdersFunction.cs
+++ SE104_project/ViewModels/FunctionViewModel/Detail Functions/OrdersFunction.cs
@@ -1,16 +1,46 @@
-﻿using SE104_OnlineShopManagement.Models.ModelEntity;
+﻿using MongoDB.Driver;
+using SE104_OnlineShopManagement.Models.ModelEntity;
 using SE104_OnlineShopManagement.Network;
+using SE104_OnlineShopManagement.Network.Get_database;
+using SE104_OnlineShopManagement.ViewModels.ComponentViewModel;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 
 namespace SE104_OnlineShopManagement.ViewModels.FunctionViewModel.Detail_Functions
 {
     class OrdersFunction : BaseFunction
     {
-        public OrdersFunction(AppSession session, MongoConnect connect) : base(session, connect)
-        { 
+        #region Properties
+        private MongoConnect _connection;
+        private AppSession _session;
+        public ObservableCollection<OrdersControlViewModel> listOrders { get; set; }
+        #endregion
 
+        #region ICommand
+        #endregion
+        public OrdersFunction(AppSession session, MongoConnect connect) : base(session, connect)
+        {
+            this._session = session;
+            this._connection = connect;
+            listOrders = new ObservableCollection<OrdersControlViewModel>();
+            GetData();
         }
+
+        #region DB
+        public async void GetData()
+        {
+            var filter = Builders<BillInformation>.Filter.Empty;
+            GetBills getter = new GetBills(_connection.client, _session, filter);
+            var ls = await getter.Get();
+            foreach (BillInformation bill in ls)
+            {
+                listOrders.Add(new OrdersControlViewModel(bill));
+            }
+            Console.Write("Executed");
+            OnPropertyChanged(nameof(listOrders));
+        }
+        #endregion
     }
 }
