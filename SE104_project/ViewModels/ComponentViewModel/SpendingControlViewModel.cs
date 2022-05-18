246291d4275475cd381c123df20efc23ca8e2928
namkhanh685 <trannamkhanh685@gmail.com>
2022-05-18 14:53:51 +0700

    Update SpendingFunction

---
+++ SE104_project/ViewModels/ComponentViewModel/SpendingControlViewModel.cs
@@ -0,0 +1,53 @@
+﻿using MongoDB.Driver;
+using SE104_OnlineShopManagement.Models.ModelEntity;
+using SE104_OnlineShopManagement.Network.Get_database;
+using SE104_OnlineShopManagement.ViewModels.FunctionViewModel;
+using SE104_OnlineShopManagement.ViewModels.FunctionViewModel.Detail_Functions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SE104_OnlineShopManagement.ViewModels.ComponentViewModel
+{
+    public class SpendingControlViewModel: ViewModelBase
+    {
+        #region Properties
+        public StockInformation stock { get; set; }
+        public string ID { get; set; }
+        public DateTime spendDay { get; set; }
+        public string User { get; set; }
+        public long total { get; set; }
+        public string displayID { get; set; }
+        private ISpendingParent _parent { get; set; }
+
+        #endregion
+        public SpendingControlViewModel(StockInformation stockinfo, ISpendingParent parent)
+        {
+            this.stock = stockinfo;
+            _parent = parent;
+            this.ID= stockinfo.ID;
+            this.spendDay = stockinfo.StockDay;
+            this.displayID = stockinfo.displayID;
+            this.total= stockinfo.total;
+            GetEmployeeName();
+        }
+        #region Function
+        public async void GetEmployeeName()
+        {
+            var filter = Builders<UserInfomation>.Filter.Eq(x => x.ID, stock.User);
+            GetUsers getter = new GetUsers((_parent as BaseFunction).Connect.client, (_parent as BaseFunction).Session, filter);
+            var ls = await getter.get();
+            if (ls != null && ls.Count > 0)
+            {
+                User = ls.First().LastName;
+                OnPropertyChanged(nameof(User));
+            }
+            else
+            {
+                return;
+            }
+        }
+        #endregion
+    }
+}
--- SE104_project/ViewModels/FunctionViewModel/Detail Functions/SpendingFunction.cs
+++ SE104_project/ViewModels/FunctionViewModel/Detail Functions/SpendingFunction.cs
@@ -1,17 +1,53 @@
-﻿using SE104_OnlineShopManagement.Models.ModelEntity;
+﻿using MongoDB.Driver;
+using SE104_OnlineShopManagement.Models.ModelEntity;
 using SE104_OnlineShopManagement.Network;
+using SE104_OnlineShopManagement.Network.Get_database;
+using SE104_OnlineShopManagement.ViewModels.ComponentViewModel;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace SE104_OnlineShopManagement.ViewModels.FunctionViewModel.Detail_Functions
 {
-    class SpendingFunction : BaseFunction
+    public interface ISpendingParent
     {
+
+    }
+    class SpendingFunction : BaseFunction, ISpendingParent
+    {
+        #region Properties
+        private MongoConnect _connection;
+        private AppSession _session;
+        public bool isLoaded { get; set; }
+        public ObservableCollection<SpendingControlViewModel> listSpending { get; set; }
+        #endregion
         public SpendingFunction(AppSession session, MongoConnect connect) : base(session, connect)
         {
-
+            this._session = session;
+            this._connection = connect;
+            listSpending = new ObservableCollection<SpendingControlViewModel>();
+            isLoaded = true;
+            GetData();
         }
-
+        #region DB
+        public async void GetData()
+        {
+            var filter = Builders<StockInformation>.Filter.Empty;
+            GetStocking getter = new GetStocking(_connection.client, _session, filter);
+            Task<List<StockInformation>> task = getter.Get();
+            var ls = await task;
+            Task.WaitAll(task);
+            foreach (StockInformation stock in ls)
+            {
+                listSpending.Add(new SpendingControlViewModel(stock, this));
+            }
+            isLoaded = false;
+            OnPropertyChanged(nameof(isLoaded));
+            Console.Write("Executed");
+            OnPropertyChanged(nameof(listSpending));
+        }
+        #endregion
     }
 }
