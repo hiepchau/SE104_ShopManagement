b42f659563e94d6eb29d3a120b0c0e20f1556947
hiepc <ch.dhiep@gmail.com>
2022-05-20 17:43:04 +0700

    Binding searchcommand warehouse

--- SE104_project/ViewModels/FunctionViewModel/Detail Functions/WareHouseFunction.cs
+++ SE104_project/ViewModels/FunctionViewModel/Detail Functions/WareHouseFunction.cs
@@ -23,6 +23,7 @@ namespace SE104_OnlineShopManagement.ViewModels.FunctionViewModel.Detail_Functio
         private AppSession _session;
         private ManagingFunctionsViewModel managingFunction;
         private ManagementMenu ManagementMenu;
+        public string searchString { get; set; }
         public ObservableCollection<WareHouseControlViewModel> listItemWareHouse { get; set; }
         #endregion
 
@@ -30,6 +31,8 @@ namespace SE104_OnlineShopManagement.ViewModels.FunctionViewModel.Detail_Functio
         public ICommand OpenImportProductsCommand { get; set; }
         public ICommand PreviousWareHousePageCommand { get; set; }
         public ICommand NextWareHousePageCommand { get; set; }
+        public ICommand SearchCommand { get; set; }
+
         #endregion
         public WareHouseFunction(AppSession session, MongoConnect connect, ManagingFunctionsViewModel managingFunctionsViewModel, ManagementMenu managementMenu) : base(session, connect)
         {
@@ -42,6 +45,8 @@ namespace SE104_OnlineShopManagement.ViewModels.FunctionViewModel.Detail_Functio
 
             _ = GetData();
             OpenImportProductsCommand = new RelayCommand<Object>(null, OpenImportProducts);
+            SearchCommand = new RelayCommand<Object>(null, search);
+
         }
 
         #region Function
@@ -50,6 +55,18 @@ namespace SE104_OnlineShopManagement.ViewModels.FunctionViewModel.Detail_Functio
             managingFunction.Currentdisplaying = new ImportProductsFunction(Session, Connect);
             ManagementMenu.changeSelectedItem(4);
             managingFunction.CurrentDisplayPropertyChanged();
+        }
+        private async void search(object o)
+        {
+            searchString = (o.ToString());
+            if (string.IsNullOrEmpty(searchString))
+            {
+                await GetData();
+            }
+            else
+            {
+                await getsearchdata();
+            }
         }
         #endregion
 
@@ -64,6 +81,19 @@ namespace SE104_OnlineShopManagement.ViewModels.FunctionViewModel.Detail_Functio
                 listItemWareHouse.Add(new WareHouseControlViewModel(pro));
             }
             Console.Write("Executed");
+            OnPropertyChanged(nameof(listItemWareHouse));
+        }
+        private async Task getsearchdata()
+        {
+            listItemWareHouse.Clear();
+            OnPropertyChanged(nameof(listItemWareHouse));
+            FilterDefinition<ProductsInformation> filter = Builders<ProductsInformation>.Filter.Eq(x => x.name, searchString);
+            var tmp = new GetProducts(_connection.client, _session, filter);
+            var ls = await tmp.Get();
+            foreach (ProductsInformation pr in ls)
+            {
+                listItemWareHouse.Add(new WareHouseControlViewModel(pr));
+            }
             OnPropertyChanged(nameof(listItemWareHouse));
         }
         #endregion
