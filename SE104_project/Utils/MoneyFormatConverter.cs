ed192a6cc2ef645bedad33c292529169e219aef6
unknown <nhtdforwork@gmail.com>
2022-05-23 20:40:13 +0700

    Update new money converter

---
+++ SE104_project/Utils/MoneyFormatConverter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+
+namespace SE104_OnlineShopManagement.Utils
+{
+    public class MoneyFormatConverter : IValueConverter
+
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string s = String.Empty;
+            if(value != null && value is long)
+            {
+                s = String.Format(new CultureInfo("en-US"), "{0:n0}", (long)value);
+                return s;
+            }
+            return s;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            long result;
+            if(value != null)
+            {
+                result = ConvertToNumber(value.ToString());
+                return result;
+            }
+            return null;
+        }
+        public long ConvertToNumber(string str)
+        {
+            long result;
+            string[] s = str.Split(',');
+            string tmp = "";
+            foreach (string a in s)
+            {
+                tmp += a;
+            }
+
+            if(long.TryParse(tmp,out result))
+            {
+                return result;
+            }
+            return result;
+        }
+    }
+}
