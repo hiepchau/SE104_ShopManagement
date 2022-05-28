4afe913410b55a8ac19139d0a9f5c3c1e84e7eb6
hiepc <ch.dhiep@gmail.com>
2022-05-28 13:18:16 +0700

    Add BillTemplate UI

---
+++ SE104_project/ViewModels/FunctionViewModel/Detail Functions/BillTemplateControlViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SE104_OnlineShopManagement.ViewModels.FunctionViewModel.Detail_Functions
+{
+    class BillTemplateControlViewModel : ViewModelBase
+    {
+        #region Properties
+        #endregion
+
+        #region ICommand
+        #endregion
+
+        #region Function
+        #endregion
+    }
+}
