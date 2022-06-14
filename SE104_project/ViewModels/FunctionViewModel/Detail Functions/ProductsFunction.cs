5057942aa2efca27e8aba9d81b622d1fe91273ac
hiepc <ch.dhiep@gmail.com>
2022-06-15 01:10:41 +0700

    BUG Supplier Search

--- SE104_project/ViewModels/FunctionViewModel/Detail Functions/ProductsFunction.cs
+++ SE104_project/ViewModels/FunctionViewModel/Detail Functions/ProductsFunction.cs
@@ -771,6 +771,7 @@ namespace SE104_OnlineShopManagement.ViewModels.FunctionViewModel.Detail_Functio
             }
             OnPropertyChanged(nameof(listActiveItemsProduct));
         }
+
         #endregion
     }
 }
