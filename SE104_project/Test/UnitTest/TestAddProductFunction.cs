3a3a85d3b188bbc4392576fb61c2dc150235277f
Khanh Tran <trannamkhanh685@gmail.com>
2022-10-23 14:44:11 +0700

    .

---
+++ SE104_project/Test/UnitTest/TestAddProductFunction.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SE104_OnlineShopManagement.Test.UnitTest
+{
+    public class TestAddProductFunction
+    {
+    }
+}
---
+++ SE104_project/Test/UnitTest/TestLoginFunction.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using SE104_OnlineShopManagement.Models.ModelEntity;
+using SE104_OnlineShopManagement.Network;
+using SE104_OnlineShopManagement.Network.Get_database;
+using SE104_OnlineShopManagement.ViewModels.Authentication;
+
+namespace SE104_OnlineShopManagement.Test.UnitTest
+{
+    [TestFixture]
+    public class TestLoginFunction
+    {
+        string companyName;
+        string password;
+        string userName;
+        AuthenticationInformation authInfo;
+        Authenticator authenticator;
+        private MongoConnect Connection;
+        [SetUp]
+        public void Setup()
+        {
+            Connection = new MongoConnect();
+            companyName = "123";
+            password = "1";
+            userName = "1";
+            authInfo = new AuthenticationInformation(userName, password, companyName);
+            authenticator = new Authenticator(authInfo, Connection.client);
+        }
+        [Test]
+        public async Task TestLogin()
+        {
+            UserInfomation userInfo = await authenticator.Authenticate();
+            bool isLogin;
+            if (userInfo != null) isLogin = true;
+            else isLogin = false;
+            Assert.IsTrue(isLogin);
+        }
+
+    }
+}
