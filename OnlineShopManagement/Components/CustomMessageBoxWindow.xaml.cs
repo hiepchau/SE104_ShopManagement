b8d27c532c7486ca213f8ff9311edb0b5d605e85
DungTri <nhtdforwork@gmail.com>
2023-01-08 14:36:03 +0700

    Update converter

--- OnlineShopManagement/Components/CustomMessageBoxWindow.xaml.cs
+++ OnlineShopManagement/Components/CustomMessageBoxWindow.xaml.cs
@@ -1,15 +1,7 @@
 ﻿using SE104_OnlineShopManagement.Utils;
 using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace SE104_OnlineShopManagement.Components
 {
---
+++ OnlineShopManagement/Utils/SelectValueConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace OnlineShopManagement.Utils
+{
+    public class SelectValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null && parameter != null)
+            {
+                int rtvalue = (int)value;
+                int rtpar = (int)parameter;
+                return (rtvalue == rtpar) ? Colors.White : Colors.Black;
+            }
+            return Colors.Black;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
