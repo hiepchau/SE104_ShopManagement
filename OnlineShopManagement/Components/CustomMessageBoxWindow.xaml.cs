9a58724489f18c27f4052f6e534d22c1c50a26a9
DungTri <nhtdforwork@gmail.com>
2023-01-08 13:47:09 +0700

    Fix dialog image bug

--- OnlineShopManagement/Components/CustomMessageBoxWindow.xaml.cs
+++ OnlineShopManagement/Components/CustomMessageBoxWindow.xaml.cs
@@ -192,19 +192,19 @@ namespace SE104_OnlineShopManagement.Components
             {
                 case MessageBoxImage.Warning:
                     //bitmapImage = new BitmapImage(new Uri("..//Resources//Images//warning.png", UriKind.Relative));
-                    bitmapImage = new BitmapImage(new Uri("pack://application:,,,/SE104_OnlineShopManagement;component/Resources/Images/warning.png"));
+                    bitmapImage = new BitmapImage(new Uri("pack://application:,,,/OnlineShopManagement;component/Resources/Images/warning.png"));
                     break;
                 case MessageBoxImage.Asterisk:
-                    bitmapImage = new BitmapImage(new Uri("pack://application:,,,/SE104_OnlineShopManagement;component/Resources/Images/succes.png"));
+                    bitmapImage = new BitmapImage(new Uri("pack://application:,,,/OnlineShopManagement;component/Resources/Images/succes.png"));
                     break;
                 case MessageBoxImage.Error:
-                    bitmapImage = new BitmapImage(new Uri("pack://application:,,,/SE104_OnlineShopManagement;component/Resources/Images/error.png"));
+                    bitmapImage = new BitmapImage(new Uri("pack://application:,,,/OnlineShopManagement;component/Resources/Images/error.png"));
                     break;
                 case MessageBoxImage.Question:
-                    bitmapImage = new BitmapImage(new Uri("pack://application:,,,/SE104_OnlineShopManagement;component/Resources/Images/help.png"));
+                    bitmapImage = new BitmapImage(new Uri("pack://application:,,,/OnlineShopManagement;component/Resources/Images/help.png"));
                     break;
                 default:
-                    bitmapImage = new BitmapImage(new Uri("pack://application:,,,/SE104_OnlineShopManagement;component/Resources/Images/info.png"));
+                    bitmapImage = new BitmapImage(new Uri("pack://application:,,,/OnlineShopManagement;component/Resources/Images/info.png"));
                     break;
             }
 
